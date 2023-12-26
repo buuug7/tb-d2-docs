a2c3f327a5c5df2119d24e987330baf320c0d003
buuug7 <youpp@126.com>
2023-12-26 12:34:50 +0800

    配置 i18N, 添加搜索插件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,43 +4,44 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '土鳖暗黑 1.13C 优化版',
-  tagline: '土鳖暗黑, 匠心打造, 年度巨献, 基于暗黑 1.13C 纯净版做了适当优化, 经典怀旧, 微变, 全职业平衡, 经典设置, 欢迎体验.',
-  favicon: 'img/icon-48x48.png',
+  title: "土鳖暗黑 1.13C 优化版",
+  tagline:
+    "土鳖暗黑, 匠心打造, 年度巨献, 基于暗黑 1.13C 纯净版做了适当优化, 经典怀旧, 微变, 全职业平衡, 经典设置, 欢迎体验.",
+  favicon: "img/icon-48x48.png",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "zh-CN",
+    locales: ["zh-CN"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -54,7 +55,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -64,62 +65,62 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: '土鳖暗黑',
+        title: "土鳖暗黑",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/icon-144x144.png',
+          alt: "土鳖暗黑 LOGO",
+          src: "img/icon-144x144.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: '战网介绍',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "siteSummary",
           },
-          {to: '/blog', label: '博客', position: 'left'},
+          { to: "/blog", label: "blog", position: "left" },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: '文档',
+            title: "Docs",
             items: [
               {
-                label: '战网介绍',
-                to: '/docs/intro',
+                label: "siteIntro",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: '社区',
+            title: "Community",
             items: [
               {
-                label: 'd2mods.info',
-                href: 'https://d2mods.info/',
+                label: "d2mods.info",
+                href: "https://d2mods.info/",
               },
               {
-                label: '暗黑破坏神 163 WIKI',
-                href: 'https://wiki.d.163.com/diablo/index.htm',
+                label: "暗黑破坏神 163 WIKI",
+                href: "https://wiki.d.163.com/diablo/index.htm",
               },
               {
-                label: 'BBS IMPK',
-                href: 'https://bbs.impk.cc/',
+                label: "BBS IMPK",
+                href: "https://bbs.impk.cc/",
               },
             ],
           },
           {
-            title: '更多',
+            title: "More",
             items: [
               {
-                label: '博客',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com',
+                label: "GitHub",
+                href: "https://github.com",
               },
             ],
           },
@@ -131,6 +132,16 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        language: ["en", "zh"],
+      },
+    ],
+  ],
 };
 
 export default config;
